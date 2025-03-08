54bf85bf00fd9cbf999971769674d30e2599829a
omkarlhr9595 <omkarlohar91@gmail.com>
2025-03-09 02:30:11 +0530

    refactor: simplify JSX structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import peep25 from "./assets/peep-25.svg";
 
 function App() {
   return (
-    <>
-      <div className="h-screen w-full grid place-items-center">
-        <div className="container h-full w-full flex flex-col items-center justify-center">
-          <img src={peep25} alt="" className="w-40 sm:w-64" />
-          <h1 className="text-bgwhite text-4xl sm:text-7xl font-body">
-            Work In Progress
-          </h1>
-        </div>
+    <div className="h-screen w-full grid place-items-center">
+      <div className="container h-full w-full flex flex-col items-center justify-center">
+        <img src={peep25} alt="" className="w-40 sm:w-64" />
+        <h1 className="text-bgwhite text-4xl sm:text-7xl font-body">
+          Work In Progress
+        </h1>
       </div>
-    </>
+    </div>
   );
 }
 
