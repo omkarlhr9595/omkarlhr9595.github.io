3e2d86ec4d5a86facf01f261f0cebc680017ca5b
omkarlhr9595 <omkarlohar91@gmail.com>
2025-03-09 17:18:10 +0530

    fix: update transition animations to use scaleX for smoother effects

--- src/hooks/Transition.tsx
+++ src/hooks/Transition.tsx
@@ -10,17 +10,17 @@ const Transition: React.FC<TransitionProps> = ({ children }) => {
     <>
       {children}
       <motion.div
-        className="fixed top-0 left-0 w-full h-screen bg-[#222] z-50 transform origin-bottom"
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 1 }}
+        className="fixed top-0 left-0 w-full h-screen bg-[#222] z-50 transform origin-right"
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX: 0 }}
+        exit={{ scaleX: 1 }}
         transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
       />
       <motion.div
-        className="fixed top-0 right-0 w-full h-screen bg-[#222] z-50 transform origin-top"
-        initial={{ scaleY: 1 }}
-        animate={{ scaleY: 0 }}
-        exit={{ scaleY: 0 }}
+        className="fixed top-0 right-0 w-full h-screen bg-[#222] z-50 transform origin-left"
+        initial={{ scaleX: 1 }}
+        animate={{ scaleX: 0 }}
+        exit={{ scaleX: 0 }}
         transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
       />
     </>
