ae4c6141b0e73b42179ab3200fc7e47e4fe799ec
omkarlhr9595 <omkarlohar91@gmail.com>
2025-03-09 17:13:52 +0530

    refactor: rename Home and Projects components for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { JSX } from "react";
-import Projects from "./pages/Projects";
-import Home from "./pages/Home";
+import HomePage from "./pages/Home";
+import ProjectsPage from "./pages/Projects";
 import { AnimatePresence } from "framer-motion";
 
 function App(): JSX.Element {
@@ -10,8 +10,8 @@ function App(): JSX.Element {
     <div className="h-screen w-full grid place-items-center overflow-hidden relative bg-black">
       <AnimatePresence mode="wait">
         <Routes key={location.pathname} location={location}>
-          <Route index path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
         </Routes>
       </AnimatePresence>
     </div>
